ce0e0db03ad8a933af60712efc4c1250ecb6fccc
sadif50 <smsujon50@gmail.com>
2022-11-08 17:19:03 +0600

    get services data from database

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -7,10 +8,33 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// Mongodb URI and Client
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.gh0wlz3.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+const run = async() => {
+    try{
+        const serviceCollection = client.db('dentCare').collection('services');
+
+        app.get('/services', async(req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+
+    }
+    finally{
+
+    }
+}
+run().catch(err => console.error(err));
+
 app.get('/', (req, res) => {
     res.send('Dent Care Server Running!');
 })
 
 app.listen(port, ()=>{
     console.log(`Dent Care Server Running on ${port}`);
-})
+});
