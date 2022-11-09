3e2d0da5dad37812ed3a6851e8eef7d72ace5bbd
sadif50 <smsujon50@gmail.com>
2022-11-09 16:24:40 +0600

    Add a service to database with post method

--- index.js
+++ index.js
@@ -36,13 +36,19 @@ const run = async() => {
             res.send(services);
         });
 
-
         // Get a Service by Id
         app.get('/services/:id', async(req, res) => {
             const id = req.params.id;
             const query = {_id: ObjectId(id)}
             const service = await serviceCollection.findOne(query);
             res.send(service);
+        });
+
+        // Add a service to database
+        app.post('/services', async(req, res) => {
+            const service = req.body;
+            const result = await serviceCollection.insertOne(service);
+            res.send(result);
         });
 
     }
