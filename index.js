23dcd84eb6a9c73fa8c30f294598c43d5958cb89
sadif50 <smsujon50@gmail.com>
2022-11-10 21:08:58 +0600

    finalize server

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+// middleware
 app.use(cors());
 app.use(express.json());
 
@@ -13,14 +14,13 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.gh0wlz3.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+// JWT Verify functions
 const verifyJWT = (req, res, next) => {
     const authHeader = req.headers.authorization;
-    console.log(authHeader)
     if(!authHeader){
         return res.status(401).send({message: 'Unauthorized access'})
     }
     const token = authHeader.split(' ')[1];
-
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded)=>{
         if(err){
             return res.status(401).send({message: 'Unauthorized access'});
@@ -35,10 +35,10 @@ const run = async() => {
         const serviceCollection = client.db('dentCare').collection('services');
         const reviewCollection = client.db('dentCare').collection('reviews');
 
-        // JWT token 
+        // JWT token API
         app.post('/jwt', async(req, res)=>{
             const user = req.body;
-            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '7d'});
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1d'});
             res.send({token});
         });
 
@@ -111,7 +111,7 @@ const run = async() => {
             res.send(reviews);
         });
 
-        // Get reviews by email
+        // Get reviews by email and verify JWT
         app.get('/review', verifyJWT, async(req, res) => {
             const decoded = req.decoded;
             if(decoded.email !== req.query.email){
